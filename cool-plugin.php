dc5d5e4b29fa35a8702e1487a43e7ba4141d73fc
Peter Suhm <peter@suhm.dk>
2015-10-28 18:20:51 +0100

    Update cool-plugin.php

--- cool-plugin.php
+++ cool-plugin.php
@@ -4,7 +4,7 @@
  * Plugin Name: Cool Plugin
  * Plugin URI: https://github.com/wppusher/Cool-Plugin
  * Description: OMG this plugin is so cool.
- * Version: 3.0.7
+ * Version: 3.0.8
  * Author: WP Pusher
  * Author URI: http://wppusher.com
  * License: GNU GENERAL PUBLIC LICENSE
